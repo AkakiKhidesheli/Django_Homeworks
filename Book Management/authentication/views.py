86f4e931cc69dd63226cde334ded37ba7dff5566
AkakiKhidesheli <khidesheli.akaki@gmail.com>
2025-02-17 01:16:13 +0400

    login/register

---
+++ Book Management/authentication/views.py
@@ -0,0 +1,44 @@
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
+from django.contrib.auth import authenticate, login, logout
+
+# Create your views here.
+
+def register_user(request):
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+        else:
+            return render(request, 'registration/registration.html', {'form': form})
+
+    else:
+        form = RegistrationForm()
+        return render(request, 'registration/registration.html', {'form': form})
+
+
+def login_user(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('book_list')
+
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'registration/login.html', {'form': form})
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('book_list')
