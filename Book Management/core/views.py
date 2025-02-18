40d6486664ca1beac2e7d593f5acfa3df700e580
AkakiKhidesheli <khidesheli.akaki@gmail.com>
2025-02-19 00:07:09 +0400

    search logic

--- Book Management/core/views.py
+++ Book Management/core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Books
 from .forms import BookForm
-
+from django.db.models import Q
 
 # Create your views here.
 
@@ -38,3 +38,20 @@ def update_book(request, book_id):
     else:
         form = BookForm(instance=book)
         return render(request, 'books/update_book.html', {'form': form})
+
+
+def search_book(request):
+    books = Books.objects.all().order_by('id')
+    if 'search' in request.GET:
+        search = request.GET['search']
+        # Using Q to combine queries for multiple fields
+        books = Books.objects.filter(
+            Q(title__icontains=search) |
+            Q(author__icontains=search) |
+            Q(description__icontains=search) |
+            Q(publication_year__icontains=search)
+        )
+    return render(request, 'books/book_list.html', {'books': books})
+
+
+
