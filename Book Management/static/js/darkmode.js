0b8578819aeb78d7f5aa32effac4ddea4c791506
AkakiKhidesheli <khidesheli.akaki@gmail.com>
2025-03-10 14:04:05 +0400

    darkmode

---
+++ Book Management/static/js/darkmode.js
@@ -0,0 +1,13 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     const themeToggle = document.getElementById("toggle");
+//     themeToggle.addEventListener("change", function () {
+//         document.body.dataset.bsTheme = this.checked ? "dark" : "light";
+//         localStorage.setItem("theme", this.checked ? "dark" : "light");
+//     });
+//
+//     // Load saved theme
+//     if (localStorage.getItem("theme") === "dark") {
+//         document.body.dataset.bsTheme = "dark";
+//         themeToggle.checked = true;
+//     }
+// });
